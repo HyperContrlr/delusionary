c00c20b62aa58800d3680100beff7cbfbc4e4297
MUKILTEO\1602247 <abel4prezz@gmail.com>
2024-12-11 13:37:48 -0800

    Scripts aint scripting... >:(

--- PORJECT hOTEL/Assets/KrestonStuf/Scripts/Keypad.cs
+++ PORJECT hOTEL/Assets/KrestonStuf/Scripts/Keypad.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class Keypad : MonoBehaviour
 {
     //☈ΩΞϟΨ
-    private bool clickable = false;
+    private bool clickable = true;
     private bool complete = false;
     private bool click1 = false;
     private bool click2 = false;
     private char key1 = 'x';
     private char key2 = 'x';
+    public string VMText = string.Empty;
 
     public void afterClick2()
     {
-
         if ((key1 == '5') && (key2 == 'l'))
         {
             complete = true;
             clickable = false;
+            VMText = char.ToString(key1) + char.ToString(key2);
         }
         else
         {
@@ -27,34 +28,26 @@ public class Keypad : MonoBehaviour
             click1 = false;
             click2 = false;
             clickable = true;
-
         }
     }
-    public void onSquareClick()
+    public void onSquareClick() 
     {
-        if (clickable = true)
+        if (clickable = true && (click1 = true) && (click2 = false))
         {
-            clickable = false;
-            
-            //char #1
-            if (click1 = false)
-            {
-                click1 = true;
-                //key1 = '';
-                clickable = true;
-            }
-            //char#2
-            if ((click1 = true) && (click2 = false))
-            {
-                click2 = true;
-                //key2 = '';
-                afterClick2();
-            }
+            click2 = true;
+            key2 = '☈';
+            afterClick2();
+        }
+        if (clickable = true && (click1 = false))
+        {
+            click1 = true;
+            key1 = '☈';
+            clickable = true;
         }
     }
     public void onOmegaClick()
     {
-
+        
     }
     public void onLinesClick()
     {
@@ -62,21 +55,29 @@ public class Keypad : MonoBehaviour
     }
     public void onBoltClick()
     {
-
+        
+    }
+    public void onForkClick()
+    {
+        
     }
     public void on1Click()
     {
-
+       
     }
     public void on2Click()
     {
-
+        
     }
     public void on3Click()
     {
-
+       
     }
     public void on4Click()
+    {
+       
+    }
+    public void on5Click()
     {
 
     }
