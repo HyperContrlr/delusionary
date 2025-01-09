03c01dc77c42613a32f115368724d0de12df321b
MUKILTEO\1717960 <extrashhadow@gmail.com>
2025-01-09 13:18:15 -0800

    New Roof And Floor

---
+++ PORJECT hOTEL/Assets/Lucy Stuff/Scripts/Dollpuzzle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class DollPuzzle : MonoBehaviour
+{
+    public Image Light1;
+    public Image Light2;
+    public Image Light3;
+    public Image Light4;
+    public Image Light5;
+
+    public void button1()
+    {
+        Light1.enabled = true;
+    }
+}
