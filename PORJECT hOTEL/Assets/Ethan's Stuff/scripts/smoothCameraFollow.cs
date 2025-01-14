94a9dbdc597aa01a638c7e0327e6b50c2ec51172
MUKILTEO\1717794 <hypercontrlrtwitch@gmail.com>
2025-01-14 11:50:49 -0800

    i did da thang

--- PORJECT hOTEL/Assets/Ethan's Stuff/scripts/smoothCameraFollow.cs
+++ PORJECT hOTEL/Assets/Ethan's Stuff/scripts/smoothCameraFollow.cs
@@ -6,6 +6,7 @@ public class smoothCameraFollow : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    public Quaternion rotationOffset;
     [Range(1, 10)]
     public float smoothFactor;
 
@@ -19,6 +20,15 @@ public class smoothCameraFollow : MonoBehaviour
         Vector3 targetPosition = target.position + offset;
         Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime);
         transform.position = smoothPosition;
+        if (target.CompareTag("rotatable"))
+        {
+            Quaternion targetRotation = target.rotation * rotationOffset;
+            transform.rotation = targetRotation;
+        }
+        else if (target.CompareTag("Player"))
+        {
+            transform.rotation = Quaternion.identity * rotationOffset;
+        }
     }
 }
 
